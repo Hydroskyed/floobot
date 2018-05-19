feca688d70d06fac0fc317355b4cd02df7973a5d
Hydroskyed <39439968+Hydroskyed@users.noreply.github.com>
2018-05-19 19:35:52 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("FloBot,Onmedev");
+    console.log("Flo bot a bien été connecté");
+    
+});
+
+bot.login("NDQ3NDA0NTE0MjMzMDI0NTEy.DeHKxA.JR-MWsSCQXci9sYBaDijLLu8_-A");
